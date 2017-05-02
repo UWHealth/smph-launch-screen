7855c34f5c2d0d9d445f92214e3742d444067d43
Daniel Stout <dstout2019@gmail.com>
2017-05-02 12:02:23 -0500

    Resolve height issue of tier__2 boxes in Safari by defining height via JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+// sizing for safari
+$(function() {
+    $(window).resize(function() {
+        var boxHeight = $('.tier__2 .panel--dust-2').height();
+        $('.panel--slide').height(boxHeight);
+    }).resize();
+});
